09fd1d03a01fa0c63bc4b31b4ec852006683bd0f
Garry028 <gmisal2002@gmail.com>
2024-04-24 21:00:28 +0530

    added second api

--- frontend/src/components/MainForm.jsx
+++ frontend/src/components/MainForm.jsx
@@ -4,14 +4,29 @@ import { removeStopwords } from "stopword";
 
 const MainForm = () => {
   const [textInput, setTextInput] = useState("");
+  const [classNum, setClassNum] = useState("");
+  const [facetFormula, setFacetFormula] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingSecond, setLoadingSecond] = useState(false);
 
   const [selectInput, setSelectInput] = useState(null);
+  const [selectInputSecond, setSelectInputSecond] = useState(null);
+
+  const [options, setOptions] = useState([]);
+  const [optionsSecond, setOptionsSecond] = useState([]);
+
   const handleTextInputChange = (event) => {
     setTextInput(event.target.value);
   };
 
   const handleSelectInputChange = (selectedOption) => {
+    console.log(selectedOption, "first dropdown selection");
+
     setSelectInput(selectedOption);
+  };
+  const handleSelectInputSecondChange = (selectedOption) => {
+    console.log(selectedOption, "second dropdown selection");
+    setSelectInputSecond(selectedOption);
   };
 
   const handleSubmit = (event) => {
@@ -20,11 +35,33 @@ const MainForm = () => {
     console.log("Select Input:", selectInput);
   };
 
-  const [options, setOptions] = useState([]);
+  const getTitleToMainSuject = async () => {
+    if (selectInput === null) {
+      return;
+    }
+    try {
+      setLoadingSecond(true);
+      const response = await fetch(
+        `https://colon-classification.onrender.com/titles_to_main_class_mapper/${selectInput?.value}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setOptionsSecond(
+        data.data.map((item) => ({ value: item.id, label: item.subject }))
+      );
+      setLoadingSecond(false);
+    } catch (error) {
+      console.log(error);
+      setLoadingSecond(false);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await fetch(
           "https://colon-classification.onrender.com/subjects/"
         );
@@ -35,13 +72,18 @@ const MainForm = () => {
         setOptions(
           data.data.map((item) => ({ value: item.id, label: item.subject }))
         );
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
-
     fetchData();
   }, []);
+
+  useEffect(() => {
+    getTitleToMainSuject();
+  }, [selectInput]);
 
   return (
     <div className="max-w-md mx-auto mt-8 p-4 border rounded-lg shadow-lg">
@@ -63,15 +105,32 @@ const MainForm = () => {
             htmlFor="select"
             className="block text-gray-700 font-bold mb-2"
           >
-            Select Input
+            Select Subject
           </label>
           <Select
             id="select"
             options={options}
             value={selectInput}
             onChange={handleSelectInputChange}
+            isLoading={loading}
           />
         </div>
+        {selectInput !== null && (
+          <div className="mb-4">
+            <label
+              htmlFor="select"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Title to main class
+            </label>
+            <Select
+              options={optionsSecond}
+              value={selectInputSecond}
+              onChange={handleSelectInputSecondChange}
+              isLoading={loadingSecond}
+            />
+          </div>
+        )}
         <button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
